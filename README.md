f57968caf0350b8aa70e1b0dccbdb90a0f94b5ff
ynegve <evgeny.sitnikov@gmail.com>
2020-02-17 08:10:36 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-peebnz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-peebnz)
---
+++ src/app/app-root.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app-root.component.html',
+  styleUrls: [ './app-root.component.css' ]
+})
+export class AppRootComponent  {}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
