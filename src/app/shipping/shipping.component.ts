fefd446cab02d11f28f7a53932b4f8590c45b84e
ynegve <evgeny.sitnikov@gmail.com>
2020-02-18 21:48:43 +0100

    2 files modified, 1 added

--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+
 
 @Component({
   selector: 'app-shipping',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shipping.component.css']
 })
 export class ShippingComponent implements OnInit {
+  shippingCosts;
 
-  constructor() { }
+  constructor(
+    private cartService: CartService
+  ) { }
 
   ngOnInit() {
+    this.shippingCosts = this.cartService.getShippingPrices();
   }
+
+
 
 }
